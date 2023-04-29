2356bbf772c2c254ebaf07789ca3c7c8dc204b23
Pronob karmoker <124451978+pronobkarmoker@users.noreply.github.com>
2023-04-30 00:59:07 +0600

    Update Main.java

--- Youtube song rating/Main.java
+++ Youtube song rating/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Song song1 = new Song("Bohemian Rhapsody", "Queen", 1975, 4.8);
+        Song song1 = new Song("E hawa", "X", 1975, 4.8);
         song1.play();
         song1.rate(5);
         song1.rate(4);
 
-        Song song2 = new Song("Stairway to Heaven", "Led Zeppelin", 1971, 4.4);
+        Song song2 = new Song("Despacito", "Y", 1971, 4.4);
         song2.play();
         song2.rate(3);
         song2.rate(5);
