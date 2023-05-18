b8121e649c9ff013cf4b8a2b6170ac2d1a679eff
Pronob karmoker <124451978+pronobkarmoker@users.noreply.github.com>
2023-05-19 00:44:27 +0600

    Delete Java method overriding directory

--- Java method overriding/Animal.java
+++
@@ -1,7 +0,0 @@
-public class Animal {
-
-    void speak() {
-        System.out.println("the animal can speak");
-    }
-
-}
--- Java method overriding/Dog.java
+++
@@ -1,8 +0,0 @@
-public class Dog extends Animal {
-
-    @Override
-    void speak() {
-        System.out.println("the dog BARK");
-    }
-
-}
